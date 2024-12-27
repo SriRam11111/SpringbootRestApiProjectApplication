3578c44f6e4cdd01b7826e72d5b2133e68d9857f
“sriram11111” <vijayaram4501@gmail.comgit config --global user.name sriram11111git config --global user.email vijayaram4501@gmail.com>
2024-12-27 16:32:45 +0530

    update api created

--- SpringbootRestAPIProject/src/main/java/com/RestAPI/Controller/StudentController.java
+++ SpringbootRestAPIProject/src/main/java/com/RestAPI/Controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,18 +29,26 @@ public class StudentController {
 		return students;
 	}
 	
-	@GetMapping("/students/{id}")
+	@GetMapping("/student/{id}")
 	public Student getStudents(@PathVariable int id){
 		//use spring JPA to get all students from DB through APIs
 		Student Student = repo.findById(id).get();
 		return Student ;
 	}
 	
-	@PostMapping("/students/add")
+	@PostMapping("/student/add")
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public void createStudent(@RequestBody Student student) {
 		repo.save(student); // can only send one by one not a list of students
-		
-		
 	}
+	
+	@PutMapping("/student/update/{id}")
+	public Student UpdateStudents(@PathVariable int id,@RequestBody Student studentDetails) {
+		Student student = repo.findById(id).get();
+		student.setName(studentDetails.getName());
+		student.setPercentage(studentDetails.getPercentage());
+		repo.save(student);
+		return student;
+	}
+	
 }
