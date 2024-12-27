58462224c79da41fdbb2e30de8eeaacd0145267c
“sriram11111” <vijayaram4501@gmail.comgit config --global user.name sriram11111git config --global user.email vijayaram4501@gmail.com>
2024-12-27 16:53:44 +0530

    delete api created

--- SpringbootRestAPIProject/src/main/java/com/RestAPI/Controller/StudentController.java
+++ SpringbootRestAPIProject/src/main/java/com/RestAPI/Controller/StudentController.java
@@ -1,14 +1,19 @@
 package com.RestAPI.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,6 +54,21 @@ public class StudentController {
 		student.setPercentage(studentDetails.getPercentage());
 		repo.save(student);
 		return student;
+	}
+	
+	@DeleteMapping("/student/delete/{id}")
+//	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public ResponseEntity<String> removeStudent(@PathVariable int id) {
+		Optional<Student> Student = repo.findById(id);
+		if (Student.isPresent()) {
+	        Student student = Student.get();
+	        repo.delete(student);
+	        return ResponseEntity.status(HttpStatus.OK).body("Student with ID " + id + " deleted successfully.");
+	    } else {
+	        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No student found with ID " + id);
+	    }
+
+		
 	}
 	
 }
