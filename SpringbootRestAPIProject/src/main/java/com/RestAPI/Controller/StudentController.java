3edc801f142a8741fd89295cf29cf0c9175cb602
“sriram11111” <vijayaram4501@gmail.comgit config --global user.name sriram11111git config --global user.email vijayaram4501@gmail.com>
2024-12-27 15:51:39 +0530

    post api created

--- SpringbootRestAPIProject/src/main/java/com/RestAPI/Controller/StudentController.java
+++ SpringbootRestAPIProject/src/main/java/com/RestAPI/Controller/StudentController.java
@@ -3,8 +3,12 @@ package com.RestAPI.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.RestAPI.demo.entity.Student;
@@ -20,7 +24,8 @@ public class StudentController {
 	@GetMapping("/students")
 	public List<Student> getAllStudents(){
 		//use spring JPA to get all students from DB through APIs
-		return repo.findAll(); 
+		List<Student> students= repo.findAll(); 
+		return students;
 	}
 	
 	@GetMapping("/students/{id}")
@@ -28,5 +33,12 @@ public class StudentController {
 		//use spring JPA to get all students from DB through APIs
 		Student Student = repo.findById(id).get();
 		return Student ;
+	}
+	
+	@PostMapping("/students/add")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void createStudent(@RequestBody Student student) {
+		repo.save(student); // can only send one by one not a list of students
+		
 	}
 }
